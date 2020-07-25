beb3b46d46d70e0f39b82a0e9dbe37cbadadfd38
Fredrick Yessaian <fredrick.06@gmail.com>
2020-07-25 15:25:19 -0400

    Initial Commit

---
+++ Workloadmanagement/src/main/java/com/hcl/hackathron/Workloadmanagement/WorkloadmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.hackathron.Workloadmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkloadmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkloadmanagementApplication.class, args);
+	}
+
+}
---
+++ Workloadmanagement/src/test/java/com/hcl/hackathron/Workloadmanagement/WorkloadmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.hackathron.Workloadmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkloadmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
